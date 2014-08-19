1b30d91ff781990314f627dec161ba1b21be2030
maxeust <max@spark.io>
2014-08-19 10:32:50 -0500

    Documented Commit
    Added documentation

--- drinkVend.ino
+++ drinkVend.ino
@@ -1,71 +1,72 @@
-// This #include statement was automatically added by the Spark IDE.
-#include "application.h"
-#include "Adafruit-MotorShield-V2/Adafruit-MotorShield-V2.h"
-#include "Adafruit-MotorShield-V2/Adafruit_PWMServoDriver.h"
 
-/*
- Visit the following url for help on connecting the motor shield
- to the spark core:
- https://community.spark.io/t/adafruit-motor-shield-v2-progress/5218
-*/
+int myMotorArray[4]; //initialize motor array so correct motor can be referenced
+int toMaking = 0; //integer representation of whether there is a drink to be made
+int sparkHandler(String command); //handler for the exposed Spark.function() that starts the ball rolling for the drink to be made.
+String currCommand; //the current command of how long each pump should be turned on for the drink to be made. e.g. "12,12,0,6"
+void strInter(String command); //helper function that transforms the string input "currCommand" of seconds to an array of integers
+void pumpControl(); //function responsible for turning the correct pumps on/off, dispensing the drink.
+unsigned long currentMillis; //current timestamp for pumpControl
+unsigned long previousMillis; //last timestamp for pumpControl
+unsigned long interval; //current interval for how long until the next pump is turned off
+int motor1 = A0;
+int motor2 = A1;
+int motor3 = A4;
+int motor4 = A5;
 
 
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-//Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M3
-Adafruit_DCMotor *myMotor0 = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor1 = AFMS.getMotor(2);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(3);
-Adafruit_DCMotor *myMotor3 = AFMS.getMotor(4);
-
-Adafruit_DCMotor* myMotorArray[4];
-
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
-
-// You can also make a stepper motor on M1/M2
-//Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 1);
-int toMaking = 0;
-String currCommand;
-void mix(String command);
-void drink();
-int handler(String command);
+SYSTEM_MODE(MANUAL); //allows for control of Spark.connect() and Spark.process()
 
 
 void setup() {
     
     Serial.begin(9600);           // set up Serial library at 9600 bps
-    Serial.println("Adafruit Motorshield v2 - DC Motor test!");
     
-    myMotorArray[0] = myMotor0;
-    myMotorArray[1] = myMotor1;
-    myMotorArray[2] = myMotor2;
-    myMotorArray[3] = myMotor3;
+    pinMode(motor1, OUTPUT);
+    pinMode(motor2, OUTPUT);
+    pinMode(motor3, OUTPUT);
+    pinMode(motor4, OUTPUT);
 
-    AFMS.begin();  // create with the default frequency 1.6KHz
-    myMotor1->setSpeed(255);
-    myMotor2->setSpeed(255);
-    myMotor3->setSpeed(255);
-    myMotor0->setSpeed(255);
-    Spark.function("mix", handler);
+    
+    myMotorArray[0] = motor1;
+    myMotorArray[1] = motor2;
+    myMotorArray[2] = motor3;
+    myMotorArray[3] = motor4;
+
+    Spark.function("mix", sparkHandler);
     Spark.variable("making", &toMaking, INT);
+    
 
 }
 
 void loop() {
-    
-    if (toMaking){
-        mix(currCommand);
-        toMaking = 0;
+
+    if (Spark.connected() == false){
+        Spark.connect();
     }
+    
+    else {
+        
+        Spark.process();
+        
+        if (toMaking){
+            strInter(currCommand);
+            toMaking = 0;
+        }
+        
+    }
+    
     
 }
 
+/*
 
-int handler(String command){
+handler is the handler function for a spark.function() "mix" exposed to the internet.
+When the function is called, currCommand is set to the string passed into the function,
+while toMaking is set to 1 so that the loop will trip, moving a step further towards a made drink by calling strInter().
+
+*/
+
+int sparkHandler(String command){
     
     currCommand = command;
     toMaking = 1;
@@ -73,51 +74,109 @@ int handler(String command){
     
 }
 
-void mix(String instructions) {
+/*
+strInter is a funtion called from the loop if toMaking has a value of 1. It takes in a string--currCommand--, which it will modify into an int array.
+E.g. "12,12,0,6" will become an array of the same integers in the same order, [12,12,0,6]. Then the process is moved forward by calling drink()
+on this array of integers.
+
+Parameters: a string of four comma separated integers, intended to be placed in an array.
+
+functions that call this function: loop()
+
+drink-making baton is passed to: drink()
+*/
+
+void strInter(String instructions) {
     
     int tymes[4];
     char *pointer = strtok(strdup(instructions.c_str()), ",");
     
     for(int i = 0; i < 4; i++){
         tymes[i] = atoi(pointer);
+        Serial.println("motor " + String(i) + " : " + String(atoi(pointer)) + " seconds");
         pointer = strtok(NULL, ",");
     }
     
-    drink(tymes);
+    pumpControl(tymes);
     
    
 }
 
-void drink(int tymes[4]) {
-    
-    int min = 1000;
-    int lastMin = 0;
-    int index;
-    
-    myMotor0->run(FORWARD);
-    myMotor1->run(FORWARD);
-    myMotor2->run(FORWARD);
-    myMotor3->run(FORWARD);
-    
+/*
+pumpControl is the function responsible for turning on/off the correct motors for the correct period of time. It does so by using the millis()
+function for timing, using an additive cumulation of seconds to turn of the motors in the correct order.
 
-    for(int j = 0; j < 4; j++){
+Parameters: a length-4 integer array of the times of each motor to be turned on for.
+
+functions that call this function: mix()
+
+once this function call is complete, the drink making process should be complete, enjoy!
+
+*/
+
+void pumpControl(int tymes[4]) {
+    
+    int min = 10000;
+    int lastMin = 0;
+    int index = 0;
+    int running = 1;
+    int newMin = 1;
+    interval = 100000;
+    int count = 0;
+    
+    previousMillis = millis();
+
+    digitalWrite(motor1, HIGH);
+    digitalWrite(motor2, HIGH);
+    digitalWrite(motor3, HIGH);
+    digitalWrite(motor4, HIGH);
+    
+    while (running) {
         
-        for(int i = 0; i < 4; i++){
+        if (newMin) {
+        
+            for(int i = 0; i < 4; i++){ //find the next smallest pump time in the array
+                    
+                if ((tymes[i] < min) && (tymes[i] >= 0)) {
+                    min = tymes[i];
+                    Serial.println(String(i) + " min : " + String(min));
+                    index = i;
+                }
             
-            if ((tymes[i] < min) && (tymes[i] >= 0)) {
-                min = tymes[i];
-                index = i;
             }
+            
+            interval = (min - lastMin) * 1000; //set interval to current smallest pump time - last smallest pump time (in ms)
+            tymes[index] = -1;
+            newMin = 0;
+            count++;
+            Serial.println(String(count) + " delay : " + String(interval));
+        }
+        
+        currentMillis = millis();
+        
+        if (currentMillis - previousMillis > interval) { //case where pump needs to be turned off
+            
+            previousMillis = currentMillis;
+            
+            digitalWrite(myMotorArray[index], LOW);
+    
+            Serial.println("Motor "+String(index + 1) + " stop");
+            lastMin = min;
+            min = 10000;
+            newMin = 1;
+    
         
         }
         
-        delay((min - lastMin) * 1000);
-        myMotorArray[index]->run(RELEASE);
-        lastMin = min;
-        tymes[index] = -1;
-    
+        if (count >= 4 && newMin == 1) {
+            running = 0;
+            Serial.println();
+            Serial.println("Done");
+            Serial.println();
+        }
     }
     
+
     
     return;
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-var ozVol = 0;
-var totVol = 0;
-var ml1 = 0;
-var ml2 = 0;
-var ml3 = 0;
-var ml4 = 0;
+var ozVol = 0; //display for the current size of the drink on the web page, in ounces
+var totVol = 0; //total volume of the drink, in milliliters
+var ml1 = 0; //parts requested of ingredient 1
+var ml2 = 0; //parts requested of ingredient 2
+var ml3 = 0; //parts requested of ingredient 3
+var ml4 = 0; //parts requested of ingredient 4
 
-var inputElems = {};
-var inputElemsStr = {};
+var inputElems = {}; //object for storing the values of the inputs in the setup panel
+var inputElemsStr = {}; //object for storing the string reference names for the inputs in the setup panel
 inputElemsStr['zero'] = "core_ID";
 inputElemsStr['one'] = "accessKey";
 inputElemsStr['two'] = "ing1Dis";
@@ -15,7 +15,7 @@ inputElemsStr['four'] = "ing3Dis";
 inputElemsStr['five'] = "ing4Dis";
 
 
-function inputHandler(id, key) {
+function inputHandler(id, key) { //handles the inputs when a "submit" is clicked, storing the value in inputElems and reflecting them on the web page
     inputElems[key] = $("#" + id).val();
 
     alert(inputElems[key]);
@@ -26,7 +26,8 @@ function inputHandler(id, key) {
     }
 };
 
-function mixHandler(){
+function mixHandler(){ //handles the click of the "mix" button by calculating how many seconds each pump should be turned on for, then calling sendApiRequest
+
 
     var totParts = ml1 + ml2 + ml3 + ml4;
 
@@ -51,7 +52,7 @@ function mixHandler(){
 
 }
 
-function sendApiRequest(v) {
+function sendApiRequest(v) { //calls the Spark function "mix" to pass the drink-making baton to the onboard hardware.
 
     var coreCall = "https://api.spark.io/v1/devices/" + inputElems['zero'] + "/mix";
     console.log('foo ' + v);
@@ -59,7 +60,7 @@ function sendApiRequest(v) {
 
 };
 
-function sizeSelect(size) {
+function sizeSelect(size) {//handles the event of a user selecting one of the three size buttons, reflecting that on the webpage
 
     totVol = size;
 
@@ -74,18 +75,9 @@ function sizeSelect(size) {
     console.log(ozVal);
 }
 
-$(document).ready(function(){
 
-    $(".btn-slide").click(function(){
-        $("#panel").slideToggle("slow");
-        $(this).toggleClass("active"); return false;
-    });
-
-});
-
-
-$(function() {
-    // setup master volume
+$(function() { //function that creates the sliders for part selection
+    
     var slider1 = $( "#master1" ).slider({
         orientation: "vertical",
         range: "min",
@@ -170,21 +162,12 @@ $(function() {
         );
 
 
-    $( "#master5" ).slider({
-        min: 0,
-        max: 300,
-        step: 50,
-        orientation: "horizontal",
-        range: "min",
-        animate: true
-    });
-
 });
 
 
 
-document.getElementById("ing1Vol").innerHTML = ml1;
-document.getElementById("ing2Vol").innerHTML = ml1;
-document.getElementById("ing3Vol").innerHTML = ml1;
-document.getElementById("ing4Vol").innerHTML = ml1;
-document.getElementById("ozDis").innerHTML = ozVol;
+document.getElementById("ing1Vol").innerHTML = ml1; //initialize ingredient 1 parts to 0 on the webpage.
+document.getElementById("ing2Vol").innerHTML = ml2; //initialize ingredient 2 parts to 0 on the webpage.
+document.getElementById("ing3Vol").innerHTML = ml3; //initialize ingredient 3 parts to 0 on the webpage.
+document.getElementById("ing4Vol").innerHTML = ml4; //initialize ingredient 4 parts to 0 on the webpage.
+document.getElementById("ozDis").innerHTML = ozVol; //initialize the size to 0 ounces on the webpage.
